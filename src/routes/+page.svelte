b5949017fb3c254c759f29666eefbef8c5c5dc54
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2024-01-15 21:20:53 +0900

    Hello World! を追加。

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<h1>Hello World!</h1>
+
+<style>
+  @import url(http://fonts.googleapis.com/earlyaccess/notosansjp.css);
+
+  * {
+    margin: 0;
+  }
+
+  h1 {
+    padding-top: 10px;
+    background-color: tomato;
+    color: black;
+    -webkit-text-stroke: white 1px;
+    text-align: center;
+    font-weight: bold;
+    font-family: "Noto Sans JP", sans-serif;
+  }
+</style>
