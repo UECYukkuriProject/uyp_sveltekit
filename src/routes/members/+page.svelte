b704129b12f60daf65e86632ef23ba84094fe176
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2024-01-19 06:08:51 +0900

    products, membersページを作成

---
+++ src/routes/members/+page.svelte
@@ -0,0 +1,9 @@
+<div>メンバー説明</div>
+
+<style>
+  div {
+    margin: 1em;
+    text-align: center;
+    font-family: "Noto Sans JP", sans-serif;
+  }
+</style>
---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,29 @@
+<div>
+  <h2>MaidIllager ~邪悪な村人の見た目が若干変わるMOD~</h2>
+  <img alt="2023-12-07_18.54.02" src="./images/2023-12-07_18.54.02.png" /><br />
+  <a href="https://github.com/UECYukkuriProject/MaidIllagerForge/wiki">
+    Forge版
+  </a>
+  <a href="https://github.com/UECYukkuriProject/MaidIllagerFabric/wiki">
+    Fabric版
+  </a>
+</div>
+
+<style>
+  div {
+    margin: 1em;
+    text-align: center;
+    font-family: "Noto Sans JP", sans-serif;
+  }
+
+  img {
+    max-width: 700px;
+    min-width: 40%;
+  }
+
+  a {
+    font-size: 2em;
+    margin: auto 2em auto;
+    text-decoration: none;
+  }
+</style>
